75d4f209c20894d490d01678cf2ea907ed2a2597
Kurogoma939 <kan.sss.939@gmail.com>
2023-07-14 23:34:44 +0900

    課題６追加

---
+++ work_6.dart
@@ -0,0 +1,22 @@
+/// ループ
+void main() {
+  final List<String> testList = ['a', 'b', 'c', 'd', 'e'];
+  final List<Map<String, dynamic>> memberList = [
+    {'name': '田中', 'age': 20},
+    {'name': '佐藤', 'age': 30},
+    {'name': '鈴木', 'age': 40},
+    {'name': '高橋', 'age': 50},
+    {'name': '渡辺', 'age': 60},
+  ];
+
+  /// Q1: forのループを使って、testListの要素を順番に出力してください
+
+  /// Q2: for-inのループを使って、testListの要素を順番に出力してください
+
+  /// Q3: forEachのループを使って、testListの要素を順番に出力してください
+
+  /// Q4: map関数を用いて、memberListの要素を順番に出力してください。
+  /// (例) 田中さんは20歳です
+
+  /// Q5: filter関数を用いて、memberListの要素のうち、30歳以上の人のみを出力してください。
+}
