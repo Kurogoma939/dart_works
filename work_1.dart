4bf389273b7538443751e8a3fd826c20091c18b7
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-18 13:16:24 +0900

    work_1　修正

--- work_1.dart
+++ work_1.dart
@@ -16,12 +16,13 @@ void main() {
   print(name2);
 
   /// Q5: pokemonという名前の定数を作成し、Pikachuという文字列を代入して出力してください。
-  const String pokemon = 'Pikachu';
+  String pokemon = 'Pikachu';
   print(pokemon);
 
   /// Q6: pokemonにRaichuという文字列を再代入して出力してください。（エラーのままでいいです）
-  pokemon = 'Raichu'
+  pokemon = 'Raichu';
   print(pokemon);
+
   /// Q7: Q4の再代入はエラーになりませんが、Q6の再代入はエラーになります。なぜでしょうか？コメントで回答してください。
   /// Q6で出力している「pokemon」は定数（const）で定義されており、定数に値を再代入することはできないためエラーとなる。
 }
